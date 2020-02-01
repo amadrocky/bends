78b1be74617b61cb58da383d49617e6c8e1c0a87
amadrocky <ak45amad@hotmail.fr>
2020-02-01 21:38:28 +0100

    Categories entity type

--- src/Form/OffersType.php
+++ src/Form/OffersType.php
@@ -2,12 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\Categories;
 use App\Entity\Offers;
 use App\Entity\Type;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,33 +17,10 @@ class OffersType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('category', ChoiceType::class, [
+            ->add('category', EntityType::class, [
                 'label' => 'Catégorie',
-                'choices' => [
-                    'Ameublement' => 'Ameublement',
-                    'Electroménager' => 'Electroménager',
-                    'Aliments' => 'Aliments',
-                    'Vêtements' => 'Vêtements',
-                    'Vêtements bébé' => 'Vêtements bébé',
-                    'Équipements bébé' => 'Équipements bébé',
-                    'Linge de maison' => 'Linge de maison',
-                    'Chaussures' => 'Chaussures',
-                    'Informatique' => 'Informatique',
-                    'Image & son' => 'Image & son',
-                    'DVD / Films' => 'DVD / Films',
-                    'CD / Musique' => 'CD / Musique',
-                    'Jeux & jouets' => 'Jeux & jouets',
-                    'Téléphonie' => 'Téléphonie',
-                    'Bricolage' => 'Bricolage',
-                    'Jardin' => 'Jardin',
-                    'Livres' => 'Livres',
-                    'Animaux' => 'Animaux',
-                    'Vélos' => 'Vélos',
-                    'Fournitures de bureau' => 'Fournitures de bureau',
-                    'Préstations de services' => 'Préstations de services',
-                    'Matériel médical' => 'Matériel médical',
-                    'Autre' => 'Autre'
-                ]
+                'class' => Categories::class,
+                'choice_label' => 'name',
             ])
             ->add('type', EntityType::class, [
                 'label' => 'Type',
