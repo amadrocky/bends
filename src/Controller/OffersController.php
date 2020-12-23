a3e09219c4c257df30549edadd74dc5231584e5b
amadrocky <ak45amad@hotmail.fr>
2020-12-23 16:59:50 +0100

    Add PDF export

--- src/Controller/OffersController.php
+++ src/Controller/OffersController.php
@@ -22,6 +22,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 
 /**
  * @Route("/offers")
@@ -424,6 +426,41 @@ class OffersController extends AbstractController
             'today' => new \DateTime(),
             'yesterday' => (new \DateTime())->modify('-1 day'),
             'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/export/{id}", name="offer_export", requirements={"id":"\d+"})
+     *
+     * @param Offers $offer
+     * @return Response
+     */
+    public function exportOffer(Offers $offer): Response
+    {
+        // Configure Dompdf according to your needs
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+        
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+        
+        // Retrieve the HTML generated in our twig file
+        $html = $this->renderView('PDF/exportOffer.html.twig', [
+            'title' => $offer->getTitle()
+        ]);
+        
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+        
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser (inline view)
+        $dompdf->stream("mypdf.pdf", [
+            "Attachment" => false
         ]);
     }
 
