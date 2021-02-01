e182f4d1d19c9797ae84fe72603b7ea6c3ad35a9
amadrocky <ak45amad@hotmail.fr>
2021-02-01 17:50:39 +0100

    Adding associations page

--- src/Controller/AssociationsController.php
+++ src/Controller/AssociationsController.php
@@ -6,11 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\AssociationsRepository;
 
+/**
+ * @Route("/associations", name="associations_")
+ */
 class AssociationsController extends AbstractController
 {
     /**
-     * @Route("/associations", name="associations")
+     * @Route("/", name="index")
      *
      * @param Request $request
      * @return Response
@@ -20,6 +24,36 @@ class AssociationsController extends AbstractController
         return $this->render('associations/index.html.twig', [
             'user' => $this->getUser(),
             'messages' => $request->getSession()->get('messages')
+        ]);
+    }
+
+    /**
+     * Add a new association
+     * 
+     * @Route("/new", name="new")
+     *
+     * @param Request $request
+     * @param AssociationsRepository $associationsRepository
+     * @return Response
+     */
+    public function new(Request $request, AssociationsRepository $associationsRepository): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $userHasAsso = false;
+
+        if (!empty($associationsRepository->findBy(['createdBy' => $user->getId(), 'workflowState' => 'active']))) {
+            $userHasAsso = true;
+        }
+
+        return $this->render('associations/new.html.twig', [
+            'user' => $user,
+            'messages' => $request->getSession()->get('messages'),
+            'association' => $userHasAsso
         ]);
     }
 }
