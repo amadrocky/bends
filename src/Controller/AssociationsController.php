9389b52a75927f55f38da7daecb3df4881ddfb5b
amadrocky <ak45amad@hotmail.fr>
2021-02-02 17:06:57 +0100

    Config form & front new association

--- src/Controller/AssociationsController.php
+++ src/Controller/AssociationsController.php
@@ -7,6 +7,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Repository\AssociationsRepository;
+use App\Entity\Associations;
+use App\Form\AssociationType;
 
 /**
  * @Route("/associations", name="associations_")
@@ -50,10 +52,80 @@ class AssociationsController extends AbstractController
             $userHasAsso = true;
         }
 
+        $session = $request->getSession();
+        $association = new Associations();
+        $form = $this->createForm(AssociationType::class, $association);
+        $form->handleRequest($request);
+        $cities = [];
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $association->setZipCode($_POST['zip']);
+            $association->setCity($_POST['filtered_cities']);
+            $association->setCreatedAt(new \DateTime());
+
+            /* Récupération du context (dep, region) */
+            $cities = $session->get('cities');
+            foreach ($cities as $city) {
+                if ($city['name'] === $_POST['filtered_cities']) {
+                    $association->setContext($city['context']);
+                }
+            }
+
+            /* Récupération de l'image */
+            $uploadDir = $_SERVER['PWD'] . '/assets/static/images/associations/';
+            $file = null;
+
+            if (isset($_FILES['img'])) {
+                $fileName = $_FILES['img']['name'];
+
+                if ($fileName !== "") {
+                    /* On renomme l'image */
+                    $extention = strrchr($fileName, ".");
+                    $fileName = 'association_image_' . uniqid() . $extention;
+
+                    $uploadFile = $uploadDir . basename($fileName);
+                    move_uploaded_file($_FILES['img']['tmp_name'], $uploadFile);
+                    $file = basename($uploadFile);
+                }
+            }
+
+            $association->setPicture($file);
+            $association->setCreatedBy($this->getUser());
+            $association->setModifiedAt(new \Datetime());
+            $association->setWorkflowState('created');
+            $entityManager->persist($association);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre association a bien été enregistrée');
+
+            return $this->redirectToRoute('associations_index');
+        }
+
+        if ($request->IsMethod('POST')) {
+            $apiRequest = json_decode(
+                file_get_contents(
+                    'https://api-adresse.data.gouv.fr/search/?q=' . intval($request->request->get('zipCode'))
+                ), true
+            );
+
+            foreach ($apiRequest['features'] as $key => $value) {
+                if ($value['properties']['type'] === 'municipality') {
+                    $cities[$key]['id'] = $value['properties']['id'];
+                    $cities[$key]['name'] = $value['properties']['label'];
+                    $cities[$key]['context'] = $value['properties']['context'];
+                }
+            }
+
+            $session->set('cities', $cities);
+        }
+
         return $this->render('associations/new.html.twig', [
             'user' => $user,
             'messages' => $request->getSession()->get('messages'),
-            'association' => $userHasAsso
+            'association' => $userHasAsso,
+            'form' => $form->createView(),
+            'cities' => $cities
         ]);
     }
 }
--- src/Form/AssociationType.php
+++ src/Form/AssociationType.php
@@ -6,6 +6,9 @@ use App\Entity\Associations;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class AssociationType extends AbstractType
 {
@@ -18,16 +21,18 @@ class AssociationType extends AbstractType
                     'placeholder' => 'Nom'
                 ]
             ])
-            ->add('description')
-            ->add('zipCode')
-            ->add('city')
-            ->add('context')
-            ->add('picture')
-            ->add('link')
-            ->add('createdAt')
-            ->add('modifiedAt')
-            ->add('workflowState')
-            ->add('createdBy')
+            ->add('description', TextareaType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Présentez votre association aux utilisateurs afin d\'en savoir plus sur votre activité et/ou vos besoins.'
+                ]
+            ])
+            ->add('link', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Site internet (facultatif)'
+                ]
+            ])
         ;
     }
 
