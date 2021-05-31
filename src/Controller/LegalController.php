dcf4e2044420908f67a8b49f34cd4592944322b9
amadrocky <ak45amad@hotmail.fr>
2021-05-31 16:36:47 +0200

    Adding legal mentions

---
+++ src/Controller/LegalController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/legal", name="legal_")
+ */
+class LegalController extends AbstractController
+{
+    /**
+     * @Route("/mentions", name="mentions")
+     * 
+     * @param Request $request
+     * @return Response
+     */
+    public function legalMentions(Request $request): Response
+    {
+        return $this->render('legal/mentions.html.twig', [
+            'user' => $this->getUser(),
+            'messages' => $request->getSession()->get('messages')
+        ]);
+    }
+}
